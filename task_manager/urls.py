5391e788d31acdb5b775206f583cc2a21df6bb28
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-01-29 21:45:44 +0530

    database included with /completed_tasks /complete_task/<id>

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import (
+    task_view,
+    add_task_view,
+    delete_task_view,
+    completed_task_view,
+    complete_task,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", task_view),
+    path("add-task/", add_task_view),
+    path("delete-task/<int:id>/", delete_task_view),
+    path("complete_task/<int:id>/", complete_task),
+    path("completed_tasks/", completed_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,36 @@
 # Add your Views Here
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+from tasks.models import Task
+
+
+def task_view(request):
+    search_term = request.GET.get("search")
+    tasks = Task.objects.filter(completed=False).filter(deleted=False)
+    if search_term:
+        tasks = tasks.filter(title__icontains=search_term)
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def add_task_view(request):
+    task_string = request.GET.get("task")
+    Task(title=task_string).save()
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task_view(request, id):
+    # soft delete task
+    Task.objects.filter(id=id).update(deleted=True)
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task(request, id):
+    # marking task as completed
+    Task.objects.filter(id=id).update(completed=True)
+    return HttpResponseRedirect("/tasks")
+
+
+def completed_task_view(request):
+    completed_tasks = Task.objects.filter(completed=True).filter(deleted=False)
+    return render(request, "completed_tasks.html", {"tasks": completed_tasks})
